dd842e2e3accf7a1ac278f00e9745ce70ee7a9fc
kongteacher <dh65432@naver.com>
2024-08-14 18:26:57 +0900

    [1-1] 순서대로 문제풀기 100% [1-2] 선택하여 문제풀기 100% [1-3] 문제 생성 100% [1-4] 문제 수정 0% [1-5] 문제 삭제 0% [1-6] 모든 문제와 답 조회 0% [2] 회차 조회 100% [3] 회차 등록 100% [4] 회차 삭제 100%

--- src/main/java/com/ohgiraffers/controller/ProblemController.java
+++ src/main/java/com/ohgiraffers/controller/ProblemController.java
@@ -1,49 +1,53 @@
 package com.ohgiraffers.controller;
 
+import com.ohgiraffers.dto.ProblemDTO;
 import com.ohgiraffers.service.ProblemService;
+import com.ohgiraffers.service.RoundService;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ProblemController {
-
+    private static final Map<Integer, List<ProblemDTO>> problemListMap = new HashMap<>();
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final ProblemService problemService = new ProblemService();
 
     public static void main(String[] args) {
+        RoundService roundService = new RoundService();
 
         try {
-            System.out.println("===== SQLD 기출문제 프로그램 =====");
             while (true) {
-                System.out.print("1. 회차 선택 : ");
-                System.out.print("2. 회차 조회 : ");
-                System.out.print("3. 회차 등록 : ");
-                System.out.print("4. 회차 삭제 : ");
-                System.out.print("5. 프로그램 종료 : ");
+                System.out.println("===== SQLD 기출문제 프로그램 =====");
+                System.out.println("1. 회차 선택");
+                System.out.println("2. 회차 조회");
+                System.out.println("3. 회차 등록");
+                System.out.println("4. 회차 삭제");
+                System.out.println("5. 프로그램 종료");
 
-                System.out.print("메뉴 선택 : ");
+                System.out.print("\r\n메뉴 선택 : ");
                 int menuChoice = Integer.parseInt(br.readLine());
 
                 switch(menuChoice) {
-                    case 1:
-                        System.out.print("SQLD 회차 번호를 입력해주세요.");
-                        int round = Integer.parseInt(br.readLine());
-                        selectRound(round); break;
-                    case 2: qustionService.selectQuestionNo(chooseNo()); break;
-                    case 3: userService.registUser(signUp()); break;
-                    case 4:
-                        System.out.println("회원 관리 프로그램을 종료합니다."); return;
+                    case 1: selectSQLRound(roundService.round(br)); break;
+                    case 2: roundService.selectRound(problemListMap); break;
+                    case 3: roundService.insertRound(roundService.round(br), problemListMap); break;
+                    case 4: roundService.deleteRound(roundService.round(br), problemListMap); break;
+                    case 5:
+                        System.out.println("기출 문제 프로그램을 종료합니다."); return;
                     default:
                         System.out.println("번호를 잘 못 입력했습니다.");
                 }
-
             }
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 
-    public static void selectRound(int round) {
+
+    public static void selectSQLRound(int round) {
         try {
             while(true) {
                 System.out.println("===== SQLD " + round + "회 =====");
@@ -53,23 +57,21 @@ public class ProblemController {
                 System.out.println("4. 문제 수정");
                 System.out.println("5. 문제 삭제");
                 System.out.println("6. 모든 문제와 답 조회");
-                System.out.println("9. 프로그램 종료");
-                System.out.print("메뉴 선택 : ");
+                System.out.println("9. 회차 선택으로 돌아가기");
+                System.out.print("\r\n메뉴 선택 : ");
                 int menuChoice = Integer.parseInt(br.readLine());
 
                 switch(menuChoice) {
-                    case 1: problemService.solve(round); break;
-                    case 2: qustionService.selectQuestionNo(chooseNo()); break;
-                    case 3: userService.registUser(signUp()); break;
-                    case 4:
-                        User selected = userService.findUserForModify(chooseNo());
-                        if (selected == null) continue;
-                        userService.modifyUser(reform(selected));
-                        break;
-                    case 5: userService.removeUser(chooseNo()); break;
-                    case 6: userService.removeUser(chooseNo()); break;
+                    case 1: problemService.orderSolveProblem(round, problemListMap); break;
+                    case 2:
+                        int problemNo = chooseProblemNo();
+                        problemService.selectSolveProblem(problemNo, problemListMap.get(round).get(problemNo - 1)); break;
+                    case 3: problemService.insertProblem(br, problemListMap.get(round)); break;
+                    // case 4: break;
+                    // case 5: break;
+                    // case 6: break;
                     case 9:
-                        System.out.println("프로그램을 종료합니다."); return;
+                        System.out.println("회차 선택으로 돌아갑니다."); return;
                     default:
                         System.out.println("번호를 잘 못 입력했습니다.");
                 }
@@ -77,7 +79,6 @@ public class ProblemController {
         } catch (Exception e) {
             System.out.println("selectRound Exception : " + e);
         }
-
     }
 
     public static int chooseProblemNo() {
